d03bf57210b221584029a0b9a3474ea94fbf9de0
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-01-27 21:28:36 +0200

    Cleanup begin work on users

--- SchoolSystem/SchoolSystem.WebForms/App_Start/Bindings/ServicesModule.cs
+++ SchoolSystem/SchoolSystem.WebForms/App_Start/Bindings/ServicesModule.cs
@@ -1,12 +1,6 @@
 ﻿using Ninject.Modules;
 using SchoolSystem.Web.Services.Auth;
 using SchoolSystem.Web.Services.Auth.Contracts;
-using SchoolSystem.WebForms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.ApplicationServices;
 
 namespace SchoolSystem.WebForms.App_Start.Bindings
 {
@@ -15,7 +9,6 @@ namespace SchoolSystem.WebForms.App_Start.Bindings
         public override void Load()
         {
             this.Bind<IAuthService>().To<AuthService>();
-            this.Bind<Startup>().ToSelf();
         }
     }
 }
