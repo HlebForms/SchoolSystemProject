bd98f7eec7a7b2ee0a4dd4666430891cff3b1f56
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-01-28 12:10:05 +0200

    Add UsersSeeder and RolesSeeder

--- SchoolSystem/SchoolSystem.Data/Migrations/Configuration.cs
+++ SchoolSystem/SchoolSystem.Data/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace SchoolSystem.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -7,6 +10,10 @@ namespace SchoolSystem.Data.Migrations
 
     public sealed class Configuration : DbMigrationsConfiguration<SchoolSystem.Data.SchoolSystemDbContext>
     {
+        private const string AdminRole = "Admin";
+        private const string TeacherRole = "Teacher";
+        private const string StudentRole = "Student";
+
         public Configuration()
         {
             this.AutomaticMigrationsEnabled = true;
@@ -15,18 +22,63 @@ namespace SchoolSystem.Data.Migrations
 
         protected override void Seed(SchoolSystem.Data.SchoolSystemDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            this.RolesSeeder(context);
+            this.UsersSeeder(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private void RolesSeeder(SchoolSystemDbContext context)
+        {
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            var roleAdmin = new IdentityRole() { Name = AdminRole };
+            var roleTeacher = new IdentityRole() { Name = TeacherRole };
+            var roleStudent = new IdentityRole() { Name = StudentRole };
+
+            if (!context.Roles.Any(role => role.Name == AdminRole))
+            {
+                roleManager.Create(roleAdmin);
+            }
+
+            if (!context.Roles.Any(role => role.Name == TeacherRole))
+            {
+                roleManager.Create(roleTeacher);
+            }
+
+            if (!context.Roles.Any(role => role.Name == StudentRole))
+            {
+                roleManager.Create(roleStudent);
+            }
+        }
+
+        private void UsersSeeder(SchoolSystemDbContext context)
+        {
+            var userStore = new UserStore<User>(context);
+            var userManager = new UserManager<User>(userStore);
+
+            if (!context.Users.Any(u => u.UserName == "admin@admin.com"))
+            {
+                var adminUser = new User { UserName = "admin@admin.com" };
+
+                userManager.Create(adminUser, "admin");
+                userManager.AddToRole(adminUser.Id, AdminRole);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "teacher@teacher.com"))
+            {
+                var teacherUser = new User { UserName = "teacher@teacher.com" };
+
+                userManager.Create(teacherUser, "teacher");
+                userManager.AddToRole(teacherUser.Id, TeacherRole);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "student@student.com"))
+            {
+                var studentUser = new User { UserName = "student@student.com" };
+
+                userManager.Create(studentUser, "student");
+                userManager.AddToRole(studentUser.Id, StudentRole);
+            }
         }
     }
 }
--- SchoolSystem/SchoolSystem.WebForms/Identity/ApplicationUserManager.cs
+++ SchoolSystem/SchoolSystem.WebForms/Identity/ApplicationUserManager.cs
@@ -28,11 +28,11 @@ namespace SchoolSystem.WebForms.Identity
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 3
-                //RequireNonLetterOrDigit = true,
-                //RequireDigit = true,
-                //RequireLowercase = true,
-                //RequireUppercase = true,
+                RequiredLength = 3,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
