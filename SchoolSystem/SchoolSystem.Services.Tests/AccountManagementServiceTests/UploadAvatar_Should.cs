f3a369c7653f85c62fdc51b859501ee8effad3ae
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-02-07 13:38:09 +0200

    Added the magic test

--- SchoolSystem/SchoolSystem.Services.Tests/AccountManagementServiceTests/UploadAvatar_Should.cs
+++ SchoolSystem/SchoolSystem.Services.Tests/AccountManagementServiceTests/UploadAvatar_Should.cs
@@ -7,6 +7,8 @@ using SchoolSystem.Data.Models;
 using NUnit.Framework;
 using Moq;
 using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SchoolSystem.Services.Tests.AccountManagementServiceTests
 {
@@ -75,6 +77,33 @@ namespace SchoolSystem.Services.Tests.AccountManagementServiceTests
             service.UploadAvatar(NonNullString, NonNullString);
 
             mockedUserRepo.Verify(x => x.GetFirst(It.IsAny<Expression<Func<User, bool>>>()), Times.Once);
+        }
+
+        [Test]
+        public void Call_GetFirst_Correct_FromUserRepo()
+        {
+            var mockedUserRepo = new Mock<IRepository<User>>();
+            var mockedUoW = new Mock<Func<IUnitOfWork>>();
+
+            var expecedUser = new User() { UserName = NonNullString };
+
+            var list = new List<User>()
+            {
+               expecedUser,
+                new User() {UserName = "alabala" }
+            };
+
+            Expression<Func<User, bool>> funcFromMethod = null;
+            Expression<Func<User, bool>> expectedString = x => x.UserName == NonNullString;
+
+            mockedUserRepo.Setup(x => x.GetFirst(It.IsAny<Expression<Func<User, bool>>>()))
+            .Callback<Expression<Func<User, bool>>>(r => funcFromMethod = r);
+
+            var service = new AccountManagementService(mockedUserRepo.Object, mockedUoW.Object);
+
+            service.UploadAvatar(NonNullString, NonNullString);
+            var result = list.FirstOrDefault(x => funcFromMethod.Compile()(x));
+            Assert.AreSame(expecedUser, result);
         }
     }
 }
