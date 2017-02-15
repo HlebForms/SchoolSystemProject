a094c0020cceb9b90bdef75fa0a0d6031e2dec64
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-02-15 15:59:13 +0200

    changes to global asax

--- SchoolSystem/SchoolSystem.WebForms/Global.asax.cs
+++ SchoolSystem/SchoolSystem.WebForms/Global.asax.cs
@@ -1,8 +1,5 @@
-﻿using SchoolSystem.Data;
-using SchoolSystem.Data.Migrations;
-using SchoolSystem.WebForms.App_Start;
+﻿using SchoolSystem.WebForms.App_Start;
 using System;
-using System.Data.Entity;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
