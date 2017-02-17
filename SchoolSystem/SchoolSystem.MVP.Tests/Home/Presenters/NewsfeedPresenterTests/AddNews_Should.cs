e7419e5f2bbaa35141b956bd1924ab12b07c284c
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2017-02-17 16:37:20 +0200

    added some tests

---
+++ SchoolSystem/SchoolSystem.MVP.Tests/Home/Presenters/NewsfeedPresenterTests/AddNews_Should.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem.MVP.Home.Presenter;
+using SchoolSystem.MVP.Home.Views;
+using SchoolSystem.Web.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.MVP.Tests.Home.Presenters.NewsfeedPresenterTests
+{
+    [TestFixture]
+    public class AddNews_Should
+    {
+        [Test]
+        public void CallAddNewsFromNewsDataService_Once_WithCorrectArguments()
+        {
+            var mockedNewsfeedView = new Mock<INewsfeedView>();
+            var mockedNewsDataService = new Mock<INewsDataService>();
+
+            var newsDataPresenter = new  NewsfeedPresenter(mockedNewsfeedView.Object, mockedNewsDataService.Object);
+        }
+    }
+}
---
+++ SchoolSystem/SchoolSystem.MVP.Tests/Home/Presenters/NewsfeedPresenterTests/Constructor_Should.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem.MVP.Home.Presenter;
+using SchoolSystem.MVP.Home.Views;
+using SchoolSystem.Web.Services.Contracts;
+
+namespace SchoolSystem.MVP.Tests.Home.Presenters.NewsfeedPresenterTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Thorw_ArgumentNullException_WithMessageContain_newsDataService_WhenNewsDataServiceIsNull()
+        {
+            var mockedNewsfeedView = new Mock<INewsfeedView>();
+
+            Assert.That(() => new NewsfeedPresenter(mockedNewsfeedView.Object, null),
+                Throws.ArgumentNullException.With.Message.Contains("newsDataService"));
+        }
+
+        [Test]
+        public void NotThrow_WhenArgumentsAreValid()
+        {
+            var mockedNewsfeedView = new Mock<INewsfeedView>();
+            var mockedNewsDataService = new Mock<INewsDataService>();
+
+            Assert.DoesNotThrow(() => new NewsfeedPresenter(mockedNewsfeedView.Object, mockedNewsDataService.Object));
+        }
+    }
+}
