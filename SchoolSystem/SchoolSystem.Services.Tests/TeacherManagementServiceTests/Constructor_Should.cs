d4953f6853267dcd3ff00015bcbd16f6b4c55278
Kiril_Velichkov <kvelichkow@gmail.com>
2017-02-20 17:47:24 +0200

    TeacherManagementService Tests

---
+++ SchoolSystem/SchoolSystem.Services.Tests/TeacherManagementServiceTests/Constructor_Should.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using SchoolSystem.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Services.Tests.TeacherManagementServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Throw_When_teachersRepo_IsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TeacherManagementService(null));
+        }
+    }
+}
---
+++ SchoolSystem/SchoolSystem.Services.Tests/TeacherManagementServiceTests/GetAllTeachers_Should.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolSystem.Data.Contracts;
+using SchoolSystem.Data.Models;
+using SchoolSystem.Data.Models.CustomModels;
+using SchoolSystem.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Services.Tests.TeacherManagementServiceTests
+{
+    [TestFixture]
+    public class GetAllTeachers_Should
+    {
+        [Test]
+        public void Call_TeacherRepo_GetAll_Method_Once()
+        {
+            var mockedTeachersRepo = new Mock<IRepository<Teacher>>();
+
+            var service = new TeacherManagementService(mockedTeachersRepo.Object);
+
+            mockedTeachersRepo.Setup(x => x.GetAll(
+                It.IsAny<Expression<Func<Teacher, bool>>>(),
+                It.IsAny<Expression<Func<Teacher, TeacherBasicInfoModel>>>(),
+                It.IsAny<Expression<Func<Teacher, object>>[]>()
+                ));
+
+            service.GetAllTeachers();
+
+            mockedTeachersRepo.Verify(x => x.GetAll(
+                It.IsAny<Expression<Func<Teacher, bool>>>(),
+                It.IsAny<Expression<Func<Teacher, TeacherBasicInfoModel>>>(),
+                It.IsAny<Expression<Func<Teacher, object>>[]>()
+                ), Times.Once);
+        }
+    }
+}
